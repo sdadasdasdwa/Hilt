c98186eb3ae1adb83f5dbeb9af43d401e7dc038a
sdadasdasdwa <3433878731@qq.com>
2023-06-27 15:22:38 +0800

    第三方类的依赖注入

--- app/src/main/java/com/example/hilt/MainActivity.kt
+++ app/src/main/java/com/example/hilt/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.hilt.dependencies.Engine
 import com.example.hilt.dependencies.Truck
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.OkHttpClient
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -13,11 +14,16 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var truck: Truck
 
+    @Inject
+    lateinit var okHttpClient:OkHttpClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        truck.deliver()
+//        truck.deliver()
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/hilt/dependencies/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.example.hilt.dependencies
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponentManager::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20,TimeUnit.SECONDS)
+            .writeTimeout(20,TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("http://example.com/")
+            .client(okHttpClient)
+            .build()
+
+    }
+}
